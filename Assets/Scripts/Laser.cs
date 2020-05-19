6fbce7268b00144f8bb1990c49a31d59ed88e731
Vu_Nam <vuducnam.vdn@gmail.com>
2020-05-19 21:24:55 +0700

    Initialize

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    int speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0,speed);
+        Destroy(gameObject, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Range(1,1000)][SerializeField]
+    int Velocity=1;
+    [SerializeField]
+    float padding = 1;
+    [SerializeField]
+    GameObject objectLaser;
+    Vector2 minV, maxV;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera mainCam = Camera.main;
+        minV = mainCam.ViewportToWorldPoint(new Vector2(0, 0)) + new Vector3(padding,padding);
+        maxV = mainCam.ViewportToWorldPoint(new Vector2(1, 1)) - new Vector3(padding, padding);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(objectLaser,transform.position,Quaternion.identity);
+        }
+    }
+    private void Move()
+    {
+         
+        var deltaX = Time.deltaTime * Input.GetAxis("Horizontal") * Velocity;
+        var newXPos = Mathf.Clamp(transform.position.x+deltaX,minV.x,maxV.x);
+        var deltaY = Time.deltaTime * Input.GetAxis("Vertical") * Velocity;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY,minV.y,maxV.y);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
